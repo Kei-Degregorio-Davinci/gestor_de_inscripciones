d667a13a423b6e09f9f731a3333f5b1642873e48
Keitty <116025068+Kei-Degregorio-Davinci@users.noreply.github.com>
2022-11-24 01:53:02 -0300

    feat: add estudiante class

--- src/Curso.java
+++ src/Curso.java
@@ -1,2 +1,52 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Curso {
+    private Date horario;
+    private Materia materia;
+    private Profesor profesor;
+    private ArrayList<Estudiante> estudiante;
+
+    public Curso(Date horario, Materia materia, Profesor profesor) {
+        this.horario = horario;
+        this.materia = materia;
+        this.profesor = profesor;
+    }
+
+    public Date getHorario() {
+        return horario;
+    }
+
+    public void setHorario(Date horario) {
+        this.horario = horario;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public Profesor getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(Profesor profesor) {
+        this.profesor = profesor;
+    }
+
+    public ArrayList<Estudiante> getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(ArrayList<Estudiante> estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public void agregarEstudiante(Estudiante estudiante){
+        
+    }
+
 }
--- src/Materia.java
+++ src/Materia.java
@@ -34,6 +34,6 @@ public class Materia {
     }
 
     public void agregarCurso(Curso curso){
-        
+
     }
 }
